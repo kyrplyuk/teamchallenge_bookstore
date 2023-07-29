556493445b7552c1bb9886862b99992d79a78a84
y.pastushenko <y.pastushenko@unkd.com.ua>
2023-07-29 19:54:14 +0300

    Added TypeOrm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import configTypeOrm from './ormconfig';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configTypeOrm),
+    UsersModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
